7348cb7fba4d4d89d11938b8ac8ecd57eee78d4d
koushikskr <60553256+koushikskr@users.noreply.github.com>
2020-06-10 15:39:49 -0500

    commiting icp1

---
+++ py icp1/source/Arithmetic operations.py
@@ -0,0 +1,11 @@
+a = int(input('Enter First number: '))
+b = int(input('Enter Second number: '))
+add = a + b
+dif = a - b
+mul = a * b
+div = a/b
+
+print('Sum of ',a ,'and' ,b ,'is :',add)
+print('Difference of ',a ,'and' ,b ,'is :',dif)
+print('Product of' ,a ,'and' ,b ,'is :',mul)
+print('Division of ',a ,'and' ,b ,'is :',div)
